126e24cc5f291fe793af3e179a3c610b9a2e411c
Gildas Cambon <gildas.cambon@ird.fr>
2008-11-19 13:17:25 +0000

    Update : New SODA opendap server
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@280 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Oforc_OGCM/make_OGCM.m
+++ Oforc_OGCM/make_OGCM.m
@@ -56,7 +56,10 @@ if strcmp(OGCM,'SODA')
 % SODA 1.4.3: Jan 2000 to Dec 2004 (QuickSCAT)
 %  url =  'http://iridl.ldeo.columbia.edu./SOURCES/.CARTON-GIESE/.SODA/.v1p4p3'
 % SODA 2.0.2-3.
-url= 'http://iridl.ldeo.columbia.edu/SOURCES/.CARTON-GIESE/.SODA/.v2p0p2-3'
+%  url=    'http://iridl.ldeo.columbia.edu/SOURCES/.CARTON-GIESE/.SODA/.v2p0p2-3'
+% SODA 2.0.2-4.
+url= 'http://iridl.ldeo.columbia.edu/SOURCES/.CARTON-GIESE/.SODA/.v2p0p2-4'
+
 disp({'!! Change from 1.4.3 version';
       '   New variables : taux, tauy, u, v';
       '   in m and m/s and no more in cm and cm/s'})
