59443ac1f5a2648a317b8c0b23fa36c251ed8d14
Gildas Cambon <gildas.cambon@ird.fr>
2010-01-11 14:27:03 +0000

    Remove mexcdf subdir to add new one, last mexcdf release, did by Hohn Evans, April 09
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@546 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- mexcdf/snctools/tests/snctools_use_mexnc.m
+++
@@ -1,8 +0,0 @@
-function tf = snctools_use_mexnc()
-
-tf = false;
-if exist('mexnc') == 2
-	tf = true;
-end
-
-
--- mexcdf/snctools/tests/snctools_use_tmw.m
+++
@@ -1,9 +0,0 @@
-function tf = snctools_use_tmw()
-
-switch ( version('-release') )
-    case { '11', '12', '13', '14', '2006a', '2006b', '2007a', '2007b', '2008a' }
-		tf = false;
-	otherwise
-		tf = true;
-end
-
