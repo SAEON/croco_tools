7defda4aafc22900c046f2e8e8536202f5bc6514
Pierrick Penven <pierrick.penven@ird.fr>
2010-01-18 11:33:56 +0000

    il faudrait coder plus propre.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@555 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Aforc_NCEP/extract_NCEP.m
+++ Aforc_NCEP/extract_NCEP.m
@@ -49,124 +49,83 @@ disp(['Get ',vname,' for year ',num2str(year),...
 % Get the variable 2D subset (take care of greenwitch)
 %
 if Get_My_Data~=1
-var=getdap(url,fname,vname,...
-            trange,level,jrange,...
-            i1min,i1max,i2min,i2max,i3min,i3max);	
-%size(var)
-%size(shiftdim(var,2))
-
+  var=getdap(url,fname,vname,...
+             trange,level,jrange,...
+             i1min,i1max,i2min,i2max,i3min,i3max);	
 %
 % Get the dataset attributes
 %
-x=readattribute([url,fname]);
-%eval(['add_offset=x.',vname,'.add_offset;']);
-%eval(['scale_factor=x.',vname,'.scale_factor;']);  %no scale scale factor and offset ??
-eval(['missing_value=x.',vname,'.missing_value;']);
-
-
-
+  x=readattribute([url,fname]);
+  eval(['missing_value=x.',vname,'.missing_value;']);
 else %if Get_My_Data
-nc=netcdf([url,fname,'.',num2str(year),'.nc']);
+  nc=netcdf([url,fname,'.',num2str(year),'.nc']);
   if ~isempty(i1min)
-      if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
-	 strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 
-	
-	var1=squeeze(nc{vname}(tndx,1,jmin:jmax,i1min:i1max));
-%        var10=flipdim(squeeze(nc{vname}(tndx,1,:,:)) , 2);
-%        var1=var10(:,jmin:jmax,i1min:i1max);
-	
-      else
-	var2=squeeze(nc{vname}(tndx,jmin:jmax,i1min:i10max));	
-%        var10=flipdim(squeeze(nc{vname}(tndx,:,:)) , 2);
-%	var1=var10(:,jmin:jmax,i1min:i1max);
-        
-      end
+    if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
+       strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 	
+      var1=squeeze(nc{vname}(tndx,1,jmin:jmax,i1min:i1max));
     else
+      var2=squeeze(nc{vname}(tndx,jmin:jmax,i1min:i10max));	
+    end
+  else
     var1=[];
   end
-  
+%
   if ~isempty(i2min)
-      if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
-	 strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 
-	
+    if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
+       strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 	
         var2=squeeze(nc{vname}(tndx,1,jmin:jmax,i2min:i2max));
-%	var20=flipdim(squeeze(nc{vname}(tndx,1,:,:)) , 2);
-%	var2=var20(:,jmin:jmax,i2min:i2max);
-	
-      else
+    else
         var2=squeeze(nc{vname}(tndx,jmin:jmax,i2min:i2max));	
-%        var20=flipdim(squeeze(nc{vname}(tndx,:,:)) , 2);
-%	var2=var20(:,jmin:jmax,i2min:i2max);
-
-      end
+    end
   else
     var2=[];
   end
   
   if ~isempty(i3min)
-       if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
-	  strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 
-	
-        var3=squeeze(nc{vname}(tndx,1,jmin:jmax,i3min:i3max));
-%	var30=flipdim(squeeze(nc{vname}(tndx,1,:,:)) , 2);
-%	var3=var30(:,jmin:jmax,i3min:i3max);
-		
-       else
+    if strcmp(vname,'air')==1  | strcmp(vname,'uwnd')==1 | ...
+       strcmp(vname,'vwnd')==1 | strcmp(vname,'shum')==1 	
+      var3=squeeze(nc{vname}(tndx,1,jmin:jmax,i3min:i3max));
+    else
         var3=squeeze(nc{vname}(tndx,jmin:jmax,i3min:i3max));
-%	var30=flipdim(squeeze(nc{vname}(tndx,:,:)) , 2);
-%	var3=var30(:,jmin:jmax,i3min:i3max);
-	
-       end
+    end
   else
     var3=[];
   end
-%%      
-    var=cat(3,var1,var2,var3);
-    close(nc)
-%%
+%     
+  var=cat(3,var1,var2,var3);
+  close(nc)
+%
   nc=netcdf([url,fname,'.',num2str(year),'.nc']);
   add_offset=nc{vname}.add_offset(:);
   if isempty(add_offset)
-     add_offset=0;    
+    add_offset=0;    
   end
   scale_factor=nc{vname}.scale_factor(:);
   if isempty(scale_factor)
-       scale_factor=1
+    scale_factor=1
   end
   missing_value=nc{vname}.missing_value(:);
   if isempty(missing_value)
-       missing_value=-99999;
+    missing_value=-99999;
   end
   close(nc)
-
+%
 end  %if Get_My_Data
 %
 % Correct the variable
 %
 if Get_My_Data~=1
-disp(['Get_My_Data is OFF'])
-var=shiftdim(var,2);
-var(var==missing_value)=NaN;
-
+  disp(['Get_My_Data is OFF'])
+  var=shiftdim(var,2);
+  var(var==missing_value)=NaN;
 else
-disp(['Get_My_Data is ON'])
-var(var==missing_value)=NaN;
-var=add_offset+var.*scale_factor;
-% $$$ %------
-% $$$ disp(['Attention car convention differenets modeles atm (NCEP) et OCEAN (roms)'])
-% $$$ disp(['size(var)= ',num2str(size(var))])
-% $$$ var=flipdim(var,2);
-% $$$ lat=flipdim(lat,1);
-% $$$ %-------
-
+  disp(['Get_My_Data is ON'])
+  var(var==missing_value)=NaN;
+  var=add_offset+var.*scale_factor;
 end  %if Get_My_Data
 %
 % Write it in a file
 %
-% $$$ size(lon)
-% $$$ size(lat)
-% $$$ size(var)
-% $$$ tndx
 write_NCEP([NCEP_dir,vname,'_Y',num2str(year),'M',num2str(month),'.nc'],...
             vname,lon,lat,time,var,Yorig)
 %
