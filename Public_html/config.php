166c1c2c139ba982189308ccbd22899eeb94c8e4
Gildas Cambon <gildas.cambon@ird.fr>
2010-10-08 17:38:04 +0000

    Cleaning and update roms.mpl.ird.fr website / download section
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@623 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Public_html/config.php
+++ Public_html/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$destinataire = "Pierrick.Penven@ird.fr,gildas.cambon@legos.obs-mip.fr";
+$destinataire = "penven.pierrick@gmail.com,gildas.cambon@legos.obs-mip.fr";
 $objet = "Inscription Roms_Agrif";
 $entetes = "From: Formulaire_RA<formulaire@ra.org>";
 
