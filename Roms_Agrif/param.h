65fb7d92bd50969b1c8f7a3e846a865d0469a8f7
Gildas Cambon <gildas.cambon@ird.fr>
2012-10-05 16:25:00 +0000

    Error : I commit this file it should not be ..., corrected!
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@908 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Roms_Agrif/param.h
+++ Roms_Agrif/param.h
@@ -126,7 +126,7 @@
       integer NSUB_X, NSUB_E, NPP
 #ifdef MPI
       integer NP_XI, NP_ETA, NNODES     
-      parameter (NP_XI=1, NP_ETA=2,  NNODES=NP_XI*NP_ETA)
+      parameter (NP_XI=1, NP_ETA=4,  NNODES=NP_XI*NP_ETA)
       parameter (NPP=1)
       parameter (NSUB_X=1, NSUB_E=1)
 #elif defined OPENMP
