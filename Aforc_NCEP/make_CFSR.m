34fcf8a8fcf8caa1365e006758a5eafa1aa757b2
Gildas Cambon <gildas.cambon@ird.fr>
2013-06-25 14:30:00 +0000

    Add processing of tidal forcing in interaanual file of type roms_frc_NCEPx_Yxx_Mxx.nc => flag "add_tides    = 1"     in romstools_param.m
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@1201 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Aforc_NCEP/make_CFSR.m
+++ Aforc_NCEP/make_CFSR.m
@@ -198,7 +198,11 @@ if makefrc==1 | makeblk==1
       % Add the tides (needs to be tested for this version of make_NCEP)
       %
       if add_tides==1
-	add_tidal_data(tidename,grdname,frcname,Y,M)
+    disp(['Add tidal data'])
+    disp(['=============='])
+	add_tidal_data(tidename,grdname,frcname,Ntides,tidalrank,...
+                       Y,M,Dmin,Hmin,Min_min,Smin,coastfileplot)
+                   
       end
       %
       % Open the ROMS forcing files
