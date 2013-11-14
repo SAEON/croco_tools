759956930b482098dc140177c7736b0ab9dc579b
Gildas Cambon <gildas.cambon@ird.fr>
2013-11-14 00:47:12 +0000

    Cleaning ...
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@1392 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Preprocessing_tools/create_bryfile.m
+++ Preprocessing_tools/create_bryfile.m
@@ -584,17 +584,6 @@ result = endef(nc);
 %
 [sc_r,Cs_r,sc_w,Cs_w] = scoordinate(theta_s,theta_b,N,hc,vtransform);
 %disp(['vtransform=',num2str(vtransform)])
-
-% cff1=1./sinh(theta_s);
-% cff2=0.5/tanh(0.5*theta_s);
-% sc_r=((1:N)-N-0.5)/N;
-% Cs_r=(1.-theta_b)*cff1*sinh(theta_s*sc_r)...
-%     +theta_b*(cff2*tanh(theta_s*(sc_r+0.5))-0.5);
-% sc_w=((0:N)-N)/N;
-% Cs_w=(1.-theta_b)*cff1*sinh(theta_s*sc_w)...
-%     +theta_b*(cff2*tanh(theta_s*(sc_w+0.5))-0.5);
-
-
 %
 % Write variables
 %
