8f6182ecd4aae459fa677e85d35ddd330ae6bc63
Pierrick Penven <pierrick.penven@ird.fr>
2013-12-11 16:38:59 +0000

    the function is called as:
        [j,i]=runoff_grid_pos(lon,lat,lonriv(k),latriv(k));
    
    and was 
    function [i,j]=runoff_grid_pos
    
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@1405 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Rivers/runoff_grid_pos.m
+++ Rivers/runoff_grid_pos.m
@@ -1,12 +1,11 @@
-function [i,j]=grid_pos(lon_grid,lat_grid,lon_real,lat_real);
+function [j,i]=grid_pos(lon_grid,lat_grid,lon_real,lat_real);
 
 %Purpose :
 % find the i and j index of a real position on the ROMS grid
 % lon_grid and lat_grid are 2D matrix : longitude and latitude of the ROMS grid
 % lon_real,lat_real : are scalars for the real position point
 %
+dist=spheric_dist(lat_grid,lat_real,lon_grid,lon_real);
+[j i]=find(dist==min(min(dist)));
 %
-    mini=min( (lon_grid-lon_real).^2 + (lat_grid-lat_real).^2 );
-    [m j]=min(mini);
-    [m i]=min( ( lon_grid(:,j)-lon_real ).^2 + ( lat_grid(:,j)-lat_real ).^2 );
 return
