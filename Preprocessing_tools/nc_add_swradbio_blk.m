dcaae9477868e9372b5d51370be8cd1110f0fa1f
Gildas Cambon <gildas.cambon@ird.fr>
2014-06-24 17:58:19 +0000

    Typo and cosmetics
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/romsagrif/Roms_tools@1566 a0f13d73-952c-0410-bb7c-e0dfd7f2369f

--- Preprocessing_tools/nc_add_swradbio_blk.m
+++ Preprocessing_tools/nc_add_swradbio_blk.m
@@ -1,5 +1,36 @@
 function nc_add_swradbio_blk(fname)
-disp(['Add radswbio: daily averaged solar short wave radiation'])
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Add radswbio (solar short-wave radiation without diurnal cycle) 
+%  field in bulk climatological atmopheric forcing file. 
+%  Variable needed by biogeochemical model PISCES
+%
+%
+%  Further Information:
+%  http://www.romsagrif.org
+%
+%  This file is part of ROMSTOOLS
+%
+%  ROMSTOOLS is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published
+%  by the Free Software Foundation; either version 2 of the License,
+%  or (at your option) any later version.
+%
+%  ROMSTOOLS is distributed in the hope that it will be useful, but
+%  WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, write to the Free Software
+%  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+%  MA  02111-1307  USA
+%
+%
+%  June 2014: G.Cambon (IRD/LEGOS)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 nw=netcdf(fname,'write');
 redef(nw)
 %
--- Preprocessing_tools/nc_add_swradbio_frc.m
+++ Preprocessing_tools/nc_add_swradbio_frc.m
@@ -1,4 +1,35 @@
 function nc_add_swradbio_frc(fname)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%
+%  Add swradbio (solar short-wave radiation without diurnal cycle) 
+%  in frc climatological atmopheric forcing file. 
+%  Variable needed by biogeochemical model PISCES
+%
+%
+%  Further Information:
+%  http://www.romsagrif.org
+%
+%  This file is part of ROMSTOOLS
+%
+%  ROMSTOOLS is free software; you can redistribute it and/or modify
+%  it under the terms of the GNU General Public License as published
+%  by the Free Software Foundation; either version 2 of the License,
+%  or (at your option) any later version.
+%
+%  ROMSTOOLS is distributed in the hope that it will be useful, but
+%  WITHOUT ANY WARRANTY; without even the implied warranty of
+%  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  GNU General Public License for more details.
+%
+%  You should have received a copy of the GNU General Public License
+%  along with this program; if not, write to the Free Software
+%  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+%  MA  02111-1307  USA
+%
+%
+%  June 2014: G.Cambon (IRD/LEGOS)
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 nw=netcdf(fname,'write');
 redef(nw)
 %
