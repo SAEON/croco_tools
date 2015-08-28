bfa500d43d7ae4a609fd61710909310e60e46d24
Patrick Marchesiello <Patrick.Marchesiello@ird.fr>
2015-08-28 19:35:22 +0200

    THACKER: upgrade consistent with Roms_Agrif

--- Run/TEST_CASES/plot_thacker_2DV.m
+++ Run/TEST_CASES/plot_thacker_2DV.m
@@ -35,11 +35,13 @@ close all
 %
 % --- model params ---
 %
-fname     = 'thacker_his.nc';            % roms file name
-g         = 9.81;                        % gravity acceleration (m^2/s)
-yindex    = 101;                         % y index
-makemovie = 0;                           % make movie using QTWriter
-makepdf   = 1;                           % make pdf file
+fname     = 'thacker_his.nc';  % roms file name
+%fname     = 'thacker_his_1D_ndtfast40.nc';
+g         = 9.81;                  % gravity acceleration (m^2/s)
+x0        = 101;
+y0        = 1;                     % x and y origins
+makemovie = 0;                     % make movie using QTWriter
+makepdf   = 0;                     % make pdf file
 %
 %======================================================================
 
@@ -67,12 +69,12 @@ for tindex=tstr:tend % ---------------------------------------------
 
 %
 % horizontal grid
- hr=squeeze(nc{'h'}(yindex,:));
+ hr=squeeze(nc{'h'}(y0,:));
  xindex=1;
  hr=hr(xindex:end);
  L=length(hr);
- xr=squeeze(nc{'x_rho'}(yindex,xindex:end));
- yr=squeeze(nc{'y_rho'}(yindex,xindex:end));
+ xr=squeeze(nc{'x_rho'}(y0,xindex:end));
+ yr=squeeze(nc{'y_rho'}(y0,xindex:end));
  dx=xr(2)-xr(1);
 %
 % vertical grid
@@ -80,7 +82,7 @@ for tindex=tstr:tend % ---------------------------------------------
  theta_s=nc.theta_s(:); 
  theta_b=nc.theta_b(:); 
  hc=nc.hc(:); 
- zeta=squeeze(nc{'zeta'}(tindex,yindex,xindex:end));
+ zeta=squeeze(nc{'zeta'}(tindex,y0,xindex:end));
  zr=squeeze(zlevs(hr,zeta,theta_s,theta_b,hc,N,'r',2));
  dzr=zr(2:end,:)-zr(1:end-1,:);               % ---> zw(2:N,:)
  zru=0.5*(zr(:,1:end-1)+zr(:,2:end));
@@ -102,38 +104,38 @@ for tindex=tstr:tend % ---------------------------------------------
  zeta1=zeta;
 
  % ... num zonal velocity ...                         ---> xu,zru
- u1=squeeze(nc{'u'}(tindex,:,yindex,xindex:end));
+ u1=squeeze(nc{'u'}(tindex,:,y0,xindex:end));
 
  % ... num meridional velocity ...                    ---> xr,zr
- v1=squeeze(nc{'v'}(tindex,:,yindex,xindex:end));
+ v1=squeeze(nc{'v'}(tindex,:,y0,xindex:end));
 
  % ... num vertical velocity ...                      ---> xr,zw
- w1=squeeze(nc{'w'}(tindex,:,yindex,xindex:end));
+ w1=squeeze(nc{'w'}(tindex,:,y0,xindex:end));
 
  % ... num temperature ...                            ---> xr,zr
- t1=squeeze(nc{'temp'}(tindex,:,yindex,xindex:end));
+ t1=squeeze(nc{'temp'}(tindex,:,y0,xindex:end));
 
  % ---------------------------------------------------------------------
  % --- compute analytical solutions (index 2) ---
  % ---------------------------------------------------------------------
- 
+
  eta = 0.1;                % --> nondimensional periodic amplitude
  D0  = 10;                 % --> max depth at rest
  Lt  = 80.e3;              % --> distance at psi points
- f   = nc{'f'}(yindex,1);  % --> 1.e-4;
+ f   = nc{'f'}(y0,1);      % --> 1.e-4;
 
- omega =  0.5*f + sqrt(0.25*f^2 +2*g*D0/Lt^2);
+ omega=sqrt(f^2 +2*g*D0/Lt^2);
 
- u2    = -eta*omega*Lt*sin(omega*time)*ones(size(u1));
- v2    = -eta*omega*Lt*cos(omega*time)*ones(size(v1));
- zeta2 =  2*eta*D0*(xr./Lt*cos(omega*time) ...
-                   -yr./Lt*sin(omega*time) - 0.5*eta) ...
-                                  .*ones(size(zeta1));
+ u2    =   -eta*omega*Lt*sin(omega*time)*ones(size(u1));
+ v2    = -f*eta*omega*Lt*cos(omega*time)*ones(size(v1));
+ zeta2 =        2*eta*D0*cos(omega*time)* ...
+                   (xr./Lt-0.5*eta*cos(omega*time)) ...
+                                    .*ones(size(zeta1));
 
  %============================================================
  % --- plot ---
  %=============================================================
- Dcrit=0.11;
+ Dcrit=0.1;
 
  xr=1.e-3*xr;
  xr2d=1.e-3*xr2d;
@@ -150,22 +152,21 @@ for tindex=tstr:tend % ---------------------------------------------
  map(nbcol/2+1,:)=[1 1 1];
  colormap(map);
 
- contourf(xr2d,zr,u1-u2,[cmin:cint:cmax]); hold on
+ contourf(xr2d,zr,u1-u2,[cmin:cint:cmax]);  hold on
  shading flat; colorbar;
- plot(xr,-hr,'color','k','LineWidth',3);
  ha=plot(xr,zeta2,'color','g','LineWidth',2);
  hn=plot(xr,zeta1,'color','r','LineWidth',2);
  legend([ha,hn],'Analytical','Numerical')
- grid on
+ plot(xr,-hr,'color','k','LineWidth',3); hold off
  axis([-100 100 -10 5])
  caxis([cmin cmax])
- thour=floor(time/3600);
+ grid on
+ thour=floor(time*24);
  title(['THACKER: U Err at Time ',num2str(thour),' hour'])
- hold off
 
- %D1=~isnan(zeta1); D2=~isnan(zeta2);
- %D=D1; if sum(D1)>sum(D2), D=D2; end
- %std(zeta2(D)-zeta1(D))
+% D1=~isnan(zeta1); D2=~isnan(zeta2);
+% D=D1; if sum(D1)>sum(D2), D=D2; end
+% std(zeta2(D)-zeta1(D))
 
  if makemovie,  
   % Write each frame to the file
@@ -192,16 +193,18 @@ end
 %=============================================================
 
 t0=nc{'scrum_time'}(1:tindex);
-u10=squeeze(nc{'u'}(1:tindex,3,yindex,yindex));
+u10=squeeze(nc{'u'}(1:tindex,3,y0,x0));
 u20=-eta*omega*Lt*sin(omega*t0);
+%u20=-f*eta*omega*Lt*cos(omega*t0*86400);
 
 figure
 t0=t0/86400;
 plot(t0,u20,'k',t0,u10,'r')
 legend('Analytical','Numerical')
+grid on
 if makepdf
  print -dpdf thacker.pdf
- eval('!pdfcrop thacker.pdf thacker_series.pdf')
+ eval('!pdfcrop thacker.pdf thacker_Useries.pdf')
 end
 
 %============================================================
@@ -220,45 +223,47 @@ tstr=1;
 
 tindex=tstr+60*2; %8*2;
 time=nc{'scrum_time'}(tindex);
-zm_6h=squeeze(nc{'zeta'}(tindex,yindex,xindex:end));
-za_6h=2*eta*D0*(xr./Lt*cos(omega*time) ...
-               -yr./Lt*cos(omega*time) - 0.5*eta) ...
+zm_6h=squeeze(nc{'zeta'}(tindex,y0,xindex:end));
+za_6h=2*eta*D0*cos(omega*time)* ...
+            (xr./Lt-0.5*eta*cos(omega*time)) ...
                               .*ones(size(zeta1));
+
 D=hr+zm_6h;
 zm_6h(D<Dcrit)=NaN;
 za_6h(za_6h<-hr)=NaN;
 
 tindex=tstr+62*2; %10*2-1;
 time=nc{'scrum_time'}(tindex);
-zm_9h=squeeze(nc{'zeta'}(tindex,yindex,xindex:end));
-za_9h=2*eta*D0*(xr./Lt*cos(omega*time) ...
-               -yr./Lt*cos(omega*time) - 0.5*eta) ...
-                             .*ones(size(zeta1));
+zm_9h=squeeze(nc{'zeta'}(tindex,y0,xindex:end));
+za_9h=2*eta*D0*cos(omega*time)* ...
+            (xr./Lt-0.5*eta*cos(omega*time)) ...
+                              .*ones(size(zeta1));
+
 D=hr+zm_9h;
 zm_9h(D<Dcrit)=NaN;
 za_9h(za_9h<-hr)=NaN;
 
 tindex=tstr+64*2; %11*2;
 time=nc{'scrum_time'}(tindex);
-zm_12h=squeeze(nc{'zeta'}(tindex,yindex,xindex:end));
-za_12h=2*eta*D0*(xr./Lt*cos(omega*time) ...
-                -yr./Lt*cos(omega*time) - 0.5*eta) ...
-                               .*ones(size(zeta1));
+zm_12h=squeeze(nc{'zeta'}(tindex,y0,xindex:end));
+za_12h=2*eta*D0*cos(omega*time)* ...
+            (xr./Lt-0.5*eta*cos(omega*time)) ...
+                              .*ones(size(zeta1));
+
 D=hr+zm_12h;
 zm_12h(D<Dcrit)=NaN;
 za_12h(za_12h<-hr)=NaN;
 
 xr=xr*1.e-3;
 figure
-plot(xr,za_6h,'k--',xr,zm_6h,'k-','LineWidth',1); hold on
+plot(xr,-hr,'color','k','LineWidth',4); hold on;
+plot(xr,za_6h,'k--',xr,zm_6h,'k-','LineWidth',1);
 text(60,zm_6h(160)+0.2,'60:00');
-legend('Analytical','Numerical')
 plot(xr,za_9h,'k--',xr,zm_9h,'k-','LineWidth',1);
 text(60,zm_9h(160)+0.2,'62:00');
 plot(xr,za_12h,'k--',xr,zm_12h,'k-','LineWidth',1);
 text(60,zm_12h(160)+0.2,'64:00');
 axis([20 100 -3 3])
-plot(xr,-hr,'color','k','LineWidth',4); hold on;
 grid on
 hold off
 title(['THACKER: numerical and analytical model sea level'])
