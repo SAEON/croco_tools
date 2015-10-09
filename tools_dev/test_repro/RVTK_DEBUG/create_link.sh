3cac11989fa95bd484d46af4b5bc64e3e7853b5a
gcambon <gildas.cambon@ird.fr>
2015-10-09 16:20:07 +0200

    mmmhhh

--- tools_dev/test_repro/RVTK_DEBUG/create_link.sh
+++ tools_dev/test_repro/RVTK_DEBUG/create_link.sh
@@ -31,6 +31,8 @@ echo 'Process namelist files'
 
 #--
 ln -sf ${dir_home}/VHR/AGRIF_FixedGrids.in.REGIONAL.VHR AGRIF_FixedGrids.in.REGIONAL
+ln -sf ${dir_home}/VORTEX/AGRIF_FixedGrids.in.VORTEX .
+
 ln -sf VHR/roms.in.VHR.1 roms.in.1
 ln -sf VHR/roms.in.VHR roms.in 
 #--
@@ -45,7 +47,6 @@ ln -sf ${dir_home}/JET/jet*.nc .
 #--
 ln -sf ${dir_home}/VORTEX/roms.in* .
 ln -sf ${dir_home}/VORTEX/vortex_*.nc* .
-ln -sf ${dir_home}/VORTEX/AGRIF_FixedGrids.in.VORTEX .
 #--
 ln -sf ${dir_home}/SHOREFACE/roms.in* .
 ln -sf ${dir_home}/SHOREFACE/shoreface*.nc .
