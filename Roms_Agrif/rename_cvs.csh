349f92e3357dca30ab0039875e2e29073b4d4507
gcambon <gildas.cambon@ird.fr>
2016-09-26 12:19:43 +0200

    Cleaning and re-organizing (split between croco and croco--tools)

--- Roms_Agrif/rename_cvs.csh
+++
@@ -1,8 +0,0 @@
-#!/bin/tcsh
-
-set old_file=t3dbc_im.F
-set new_file=t3dbc.F
-
-rm -f $old_file
-cvs remove $old_file
-cvs add $new_file
--- tools_dev/test_repro/RVTK_DEBUG_src/archives/Enchain_RVTK.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Compute the RVTK for analytical case"
-./Scripts/testromsagrif_noagrif.sh
-echo " "
-echo "Compute the RVTK Vortex + Regional"
-./Scripts/testromsagrif.sh
-echo " "
