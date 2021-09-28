896a4db2e0f3baad47a6724200b441f5dafb57f8
mathieulc <mathieu.lecorre@univ-brest.fr>
2021-09-28 16:03:08 +0000

    update namlist toy

--- Coupling_tools/script_run_cpl_inter/routines/namelists/namelist_toy.sh
+++ Coupling_tools/script_run_cpl_inter/routines/namelists/namelist_toy.sh
@@ -4,15 +4,6 @@
 # type
 export toytype=("wav" "atm") #oce,atm,wav
 
-# namelist
-
-# Time steps
-### TOY ###
-#!!!! WARNING !!!!
-# When using toy, please put the output frequency of "toyfile" in the TSP of the model
-# example: ww3.200501.nc output_frequency=3600 -----> TSP_WW3=3600
-#!!!!!!!!!!!!!!!!!
-
 # forcing files
 export toyfile=("$CWORK/toy_files/ww3.frc.200501.nc" "$CWORK/toy_files/wrfout_d01_20050101_20050131.nc")
 export timerange=('1,745' '2,125')
