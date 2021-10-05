951f4064521686bd9b2df12ba118a0db409d30b9
Swen JULLIEN <swen.jullien@ifremer.fr>
2021-10-05 10:48:31 +0000

    few changes for generalization and cosmetic changes in create_config and myjob

--- Coupling_tools/script_run_cpl_inter/myjob.sh
+++ Coupling_tools/script_run_cpl_inter/myjob.sh
@@ -2,48 +2,55 @@
 ############################ USER CHANGES ######################################
 ################################################################################
 
-# Real job duration in sec (converted to MACHINE format in submit job)
+#-------------------------------------------------------------------------------
+# Job submission settings
+#-------------------------------------------------------------------------------
+# Job walltime 
 export TIMEJOB=1800
 
-# Project Id, on which hours will be taken (No need on datarmor)
+# Project Id (on which hours are taken, if needed)
 export projectid=""
 
 #-------------------------------------------------------------------------------
 # Run date settings
 #-------------------------------------------------------------------------------
-# The experiment is divided into jobs: 1 experiment / n jobs 
+# Your run can be divided into several jobs (e.g.: 1 year run into 12 jobs of 1 month)
 
 # Full Experiment 
-#                                                            Start of Experiment
+# Start of Experiment
 export YEAR_BEGIN_EXP=2005
 export MONTH_BEGIN_EXP=1
 export DAY_BEGIN_EXP=1
-#                                                     Duration of the Experiment
+# Duration of the Experiment
 export EXP_DUR_MTH=$(( 3 * 1 ))
 export EXP_DUR_DAY=0
-#                                                                  Period of Job
+
+# Start date of the first Job
 export YEAR_BEGIN_JOB=2005
 export MONTH_BEGIN_JOB=1
 export DAY_BEGIN_JOB=1
-#                                                            Duration of the Job
+# Duration of each Job
 export JOB_DUR_MTH=1
 export JOB_DUR_DAY=0
 
 #-------------------------------------------------------------------------------
-#  Job submission type
+# Job submission type
 #-------------------------------------------------------------------------------
-export CHAINED_JOB="FALSE" #If TRUE  , place all the jobs in the queue at the begining,
-                          #Ff FALSE , place job in the queue after the previous one ended
+export CHAINED_JOB="FALSE" #If TRUE , place all the jobs in the queue at the begining,
+                           #If FALSE, place job in the queue after the previous one ended
 
-# MODE_TEST: extension du nom de l'experience
-#            pour tourner differents tests dans la meme experience
-# on peut lancer plusieurs tests en même temps mais pas être en production et lancer des tests
-export         MODE_TEST=""    #   mode Production 
+# MODE_TEST: 
+# The MODE_TEST env variable is a suffix of the experiment name
+# in order to run several tests within one experiment.
+# Several tests can be launched simultaneously 
+# However, production mode and test mode cannot be used simultaneously
+export MODE_TEST=""    #   mode Production 
 
 #-------------------------------------------------------------------------------
-#  Multi-Step
+# Multi-Step
 #-------------------------------------------------------------------------------
-LOADL_STEP_NAME="XXX" # to access directly to "get_file"/"run_model"/"put_file" in job_base.sh 
+LOADL_STEP_NAME="XXX"   # XXX will do all steps
+                        # otherwise "get_file"/"run_model"/"put_file" individual steps can be selected 
 
 # DEBUG for test
 export SCRIPT_DEBUG="FALSE"
@@ -51,10 +58,9 @@ export SCRIPT_DEBUG="FALSE"
 #-------------------------------------------------------------------------------
 # Number of core used
 # ------------------------------------------------------------------------------
-
-MPI_LAUNCH_CMD=$MPI_LAUNCH  # ccc_mprun :for irene / $MPI_LAUCH for datarmor (or mpiexec.hydra )
-export SERIAL_LAUNCH_WAV="$MPI_LAUNCH -n 1 " # for ww3 prepro getrst_ww3.sh
-
+# mpi launch command: ccc_mprun :for irene / $MPI_LAUNCH for datarmor (or mpiexec.hydra )
+MPI_LAUNCH_CMD=$MPI_LAUNCH  
+export SERIAL_LAUNCH_WAV="$MPI_LAUNCH -n 1 " # serial launch for WW3 prepro: getrst_ww3.sh
 # nb of CPUs for each model
 export NP_OCEX=2
 export NP_OCEY=2
@@ -64,7 +70,7 @@ export NP_TOY=2
 export NP_XIOS_ATM=1
 export NP_XIOS_OCE=1
 
-# additional MPI Settings for ATM 
+# additional MPI Settings for ATM (WRF)
 export atm_nprocX=-1      # -1 for automatic settings
 export atm_nprocY=-1      # -1 for automatic settings
 export atm_niotaskpg=0    # 0 for default settings
@@ -90,7 +96,7 @@ export                                ROOT_NAME_3="${DATE_END_JOB}${MODE_TEST}"
 export    jobname="job_${ROOT_NAME_1}.sh"  # File submitted. For DATRAMOR the extension is changed by .pbs
 
 #-------------------------------------------------------------------------------
-#  define the function to get/put files
+#  Define the functions to get/put files
 #-------------------------------------------------------------------------------
 export io_getfile="lnfile"
 export io_putfile="mvfile"
