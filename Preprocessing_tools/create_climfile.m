233b949affb50f958362afb10968450c207c8a30
Andres Sepulveda <andres@dgeo.udec.cl>
2022-01-28 22:47:42 +0000

    Update create_climfile.m - Missing ; created issues with Octave 4.2.2/octcdf 1.1.10

--- Preprocessing_tools/create_climfile.m
+++ Preprocessing_tools/create_climfile.m
@@ -203,7 +203,7 @@ if (vtransform ==1)
     nc{'sc_r'}.standard_name = 'ocean_s_coordinate_g1';
 elseif (vtransform ==2)
     nc{'sc_r'}.standard_name = ncchar('ocean_s_coordinate_g2');
-    nc{'sc_r'}.standard_name = 'ocean_s_coordinate_g2'     
+    nc{'sc_r'}.standard_name = 'ocean_s_coordinate_g2';     
 end
 nc{'sc_r'}.formula_terms = ncchar('s: s_rho C: Cs_r eta: zeta depth: h depth_c: hc');
 nc{'sc_r'}.formula_terms = 's: s_rho C: Cs_r eta: zeta depth: h depth_c: hc';
